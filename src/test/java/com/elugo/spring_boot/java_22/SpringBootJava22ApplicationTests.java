f0c5e4d732c75d18fa0866e03cc0a5eddcef6e1f
Emerson Lugo <emersonlugo@MacBook-Pro-de-Emerson.local>
2024-08-24 17:56:14 -0300

    Initial project configuration

---
+++ src/test/java/com/elugo/spring_boot/java_22/SpringBootJava22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elugo.spring_boot.java_22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJava22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
